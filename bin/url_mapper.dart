ca462a6916bb3ec5f8b7c07479175af75a4ae14b
LIMSUNG GUK <limasdf@gmail.com>
2015-02-11 01:46:39 +0900

    WIP

--- bin/url_mapper.dart
+++ bin/url_mapper.dart
@@ -1,21 +1,12 @@
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 import 'dart:io';
+import 'package:mime/mime.dart' as mime;
 
 void main() {
-  final Map<String, String> _headers = {'Access-Control-Allow-Origin': '*',
-                                        'Content-Type': 'text/html'};
-  shelf.Response _options(shelf.Request request) => (request.method == 'OPTIONS') ?
-      new shelf.Response.ok(null, headers: _headers) : null;
-  
-  shelf.Response _cors(shelf.Response response) => response.change(headers: _headers);
-
-  shelf.Middleware _fixCORS = shelf.createMiddleware(
-      requestHandler: _options, responseHandler: _cors);
-  
-  var handler = const shelf.Pipeline().addMiddleware(shelf.logRequests())
-      .addMiddleware(_fixCORS)
-    .addHandler(_echoRequest);
+  var handler = const shelf.Pipeline()
+      .addMiddleware(shelf.logRequests())
+      .addHandler(_echoRequest);
 
   io.serve(handler, 'localhost', 8088).then((server) {
     print('Serving at http://${server.address.host}:${server.port}');
@@ -28,11 +19,21 @@ shelf.Response _echoRequest(shelf.Request request) {
     return new shelf.Response.ok(json);
   }
 
-  final String resultPath = request.url.toString() == '/' ? '/index.html' : request.url.toString();
-        final File file = new File('./${resultPath}');
-        if (file.existsSync()) {
-          return new shelf.Response.ok(file.openRead());
-        };
+  var path = request.url.path;
+  Map headers;
+  var contentType = mime.lookupMimeType(path);
+  if (contentType != null) {
+    headers = <String, String>{
+      "Content-Type": contentType
+    };
+  }
+
+  final String resultPath =
+    request.url.toString() == '/' ? '/index.html' : request.url.toString();
+  final File file = new File('./web/${resultPath}');
+  if (file.existsSync()) {
+    return new shelf.Response.ok(file.openRead(), headers: headers);
+  };
        
   return new shelf.Response.ok('Request for "${request.url}"');
 }
