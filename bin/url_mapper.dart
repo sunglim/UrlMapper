a192b81ba0f1d0b043c4b482ae9415e35e845cde
LIMSUNG GUK <limasdf@gmail.com>
2015-02-10 22:52:21 +0900

    IT SHOULD WORK

--- bin/url_mapper.dart
+++ bin/url_mapper.dart
@@ -1,9 +1,20 @@
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
-import 'package:dart-sqlite/sqlite.dart' as sqlite;
+import 'dart:io';
 
 void main() {
+  final Map<String, String> _headers = {'Access-Control-Allow-Origin': '*',
+                                        'Content-Type': 'text/html'};
+  shelf.Response _options(shelf.Request request) => (request.method == 'OPTIONS') ?
+      new shelf.Response.ok(null, headers: _headers) : null;
+  
+  shelf.Response _cors(shelf.Response response) => response.change(headers: _headers);
+
+  shelf.Middleware _fixCORS = shelf.createMiddleware(
+      requestHandler: _options, responseHandler: _cors);
+  
   var handler = const shelf.Pipeline().addMiddleware(shelf.logRequests())
+      .addMiddleware(_fixCORS)
     .addHandler(_echoRequest);
 
   io.serve(handler, 'localhost', 8088).then((server) {
@@ -12,13 +23,16 @@ void main() {
 }
 
 shelf.Response _echoRequest(shelf.Request request) {
-  var c = new sqlite.Database("/Users/limsungguk/program_store/trash/sqlite/ex1");
-    sqlite.Row adidas = c.first("SELECT * FROM tbl1", (row) {
-      var nike = row;
-    });
   if (request.url.toString() == "/GetAll") {
     String json = "{'adidas': 'nike'}";
     return new shelf.Response.ok(json);
   }
+
+  final String resultPath = request.url.toString() == '/' ? '/index.html' : request.url.toString();
+        final File file = new File('./${resultPath}');
+        if (file.existsSync()) {
+          return new shelf.Response.ok(file.readAsStringSync());
+        };
+       
   return new shelf.Response.ok('Request for "${request.url}"');
 }
