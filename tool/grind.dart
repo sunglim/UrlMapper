0757f5228b71f48986a63137c14f5ed136a0013a
sungguk <limasdf@gmail.com>
2015-02-13 17:18:14 +0900

    added grinder running server task

--- tool/grind.dart
+++ tool/grind.dart
@@ -3,6 +3,7 @@ import 'package:grinder/grinder.dart';
 void main([List<String>  args]) {
   task('init', init);
   task('build', build, ['init']);
+  task('run_server', run, ['build']);
 
   startGrinder(args);
 }
@@ -16,6 +17,11 @@ init(GrinderContext context) {
 
 build(GrinderContext context) {
   _build_sqlite(context);
+  Pub.build(context, workingDirectory: 'front');
+}
+
+run(GrinderContext context) {
+  runDartScript(context, 'url_mapper.dart', workingDirectory: 'bin');
 }
 
 _build_sqlite(GrinderContext context) {
