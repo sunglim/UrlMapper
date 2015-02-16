8950dffce7f12b3ab1f843ae56c32fb8f8514dfe
LIMSUNG GUK <limasdf@gmail.com>
2015-02-16 21:09:16 +0900

    Server host 0.0.0.0

--- bin/url_mapper.dart
+++ bin/url_mapper.dart
@@ -4,15 +4,14 @@ import 'dart:io';
 import 'package:mime/mime.dart' as mime;
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
-import 'package:url_mapper/json_generator.dart';
 import 'package:url_mapper/controller.dart' as controller;
+import 'package:url_mapper/json_generator.dart';
 
 void main() {
   var handler = const shelf.Pipeline().addMiddleware(shelf.logRequests())
                                       .addHandler(_jsonRequest);
 
-  //io.serve(handler, '192.168.0.6', 8088).then((server) {
-  io.serve(handler, '192.168.1.103', 8088).then((server) {
+  io.serve(handler, '0.0.0.0', 8088).then((server) {
     print('Serving at http://${server.address.host}:${server.port}');
   });
 }
