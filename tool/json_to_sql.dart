a41f6e4040aaa2b6144482c6941da2b367dc145b
LIMSUNG GUK <limasdf@gmail.com>
2015-02-16 21:51:39 +0900

    Update table

--- tool/json_to_sql.dart
+++ tool/json_to_sql.dart
@@ -5,8 +5,17 @@ main([List<String> args]) {
   File data = args.isEmpty ? new File('data.json') : new File(args[0]);
   String raw = data.readAsStringSync();
   Map site_map = JSON.decode(raw)['sites'];
+  print("""
+CREATE TABLE ua_spoof(
+  ID INTEGER PRIMARY KEY AUTOINCREMENT,
+  SITE VARCHAR(255) UNIQUE,
+  KIND VARCHAR(24),
+  STATUS INTEGER NOT NULL
+);
+""");
   site_map.forEach((k,v) {
     (v as List).forEach((site)
-      => print("INSERT INTO ua_spoof (SITE, KIND) values('${site}', '${k}');"));
+      => print("INSERT INTO ua_spoof (SITE, KIND, STATUS) values('${site}', '${k}', 1);"));
   });
+  print("SELECT * from ua_spoof;");
 }
